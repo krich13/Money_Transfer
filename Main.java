0fd1808914c8348684520f259153a2dc56ad3f70
Christine <86053214+krich13@users.noreply.github.com>
2021-07-19 23:06:57 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+`public class Main {public static void main(String[] args) {
+    int current_balance = 2_000_000_000;
+    int transferring_amount = 500_000_000;
+    int total;
+    total = transferring_amount + current_balance;
+    System.out.println(total);
+}
+}'
